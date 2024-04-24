157811b80c1214535ec12173331aa168614e719c
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2024-04-24 15:12:17 -0500

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Teodoro Calle Lara on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
